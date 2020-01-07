6a5bc83638eaec53a5d2341a4f581728ef13c93d
Alchemy <Alchemy@192.168.1.189>
2020-01-07 12:24:19 +0530

    07_01_2020

---
+++ 07_01_2020/src/com/task/Check.java
@@ -0,0 +1,12 @@
+package com.task;
+
+public class Check {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BoxingUnboxingDemo demo=new BoxingUnboxingDemo(new Integer(100), 200);
+		demo.display();
+
+	}
+
+}
